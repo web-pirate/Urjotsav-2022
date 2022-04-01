8bdec7cfef0af81c3c293e162814e7ac62ac44a0
Tejas Navalkhe <navalkhetejas18@gmail.com>
2022-04-01 21:57:56 +0530

    changes

--- Urjotsav/config.py
+++ Urjotsav/config.py
@@ -1,7 +1,7 @@
 class Config:
     SECRET_KEY = '2f14f2997d0cb0ac2fd939f11c8d38f074139fde1fcaa84c86d2c29199669d4976bb0ac05e48ec817c10c7637930af700a6a'
-    # SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/tejearning'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///urjotsav.db'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@localhost/urjotsav'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////urjotsav.db'
     APP_Id = 'cf0100117d'
     APP_SECRET = '53de360c128a7f185c57e5a675a1404a90527ed6e997d789cc92985d01861684'
 
--- Urjotsav/main/views.py
+++ Urjotsav/main/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 # from Urjotsav.main.forms import BecomePartnerForm, ContactsForm
 from Urjotsav.models import User
-from flask_login import current_user
+from flask_login import current_user, logout_user, login_user, login_required
 from Urjotsav import db
 from datetime import datetime, timedelta
 
@@ -12,7 +12,25 @@ def home():
   """Home Page"""
   return render_template('index.html')
 
+@main.route('/')
+def about():
+  """Home Page"""
+  return render_template('about.html')
+
+
+@main.route('/register/')
+def register():
+    return render_template('registration.html')
+
+
 @main.route('/login/', methods=['GET', 'POST'])
 def login():
   # if request
   return render_template('login.html')
+
+
+@main.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
