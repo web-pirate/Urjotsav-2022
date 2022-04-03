1821c8e33fc44469ef30dda13f19632ebe603335
Tejas Navalkhe <navalkhetejas18@gmail.com>
2022-04-03 09:11:15 +0530

    changes

--- Urjotsav/config.py
+++ Urjotsav/config.py
@@ -9,5 +9,5 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'navalkhetejas18@gmail.com'
-    MAIL_PASSWORD = '#PythonProgramming'
+    MAIL_USERNAME = 'email@gmail.com'
+    MAIL_PASSWORD = 'pass'
